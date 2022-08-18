8f2b8cedb98dea7858433c9b3f6e5c5e222af4af
cs <siwon.cezary@gmail.com>
2022-08-18 14:00:14 +0200

    init

---
+++ src/test/java/sda/aprjavapl5/dockerdemo/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package sda.aprjavapl5.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@AutoConfigureMockMvc
+@WebMvcTest
+@ContextConfiguration(classes = {HomeController.class})
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testResponse() throws Exception{
+        this.mvc.perform(get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
